7b6c87ee6a3631a823c43044acc5c71e8d55f1b8
AlejandroIngenieria <2995019300101@ingenieria.usac.edu.gt>
2026-07-15 15:56:34 -0600

    implementacion de favoritos y vista -perfil_medico

--- hooks/use-resenas.ts
+++ hooks/use-resenas.ts
@@ -2,7 +2,6 @@
 
 import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
-import { fetchResenasPorMedico } from '@/services/resenas';
 import type { ResenaDto } from '@/types';
 
 type SessionWithAccess = {
